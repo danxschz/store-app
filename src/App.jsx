ed7d7d091931ae62890348776c1a11bfb5f30156
Daniel Sanchez <danxschz@gmail.com>
2022-10-22 23:04:09 -0400

    Set up for deployment using a custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter basename="/store-app">
+    <BrowserRouter>
       <ScrollToTop />
       <div className="App">
         <InfoBar />
--- src/pages/ThankYou/ThankYou.jsx
+++ src/pages/ThankYou/ThankYou.jsx
@@ -8,11 +8,11 @@ const ThankYou = () => {
   return (
     <main className={styles.main}>
       <div className={styles.content}>
-        <SectionHeader text="Thank you for checking out my project" />
+        <SectionHeader text="Thank you for checking out this prototype" />
         <div className={styles.info}>
           <p>This section is currently a <strong>WIP</strong></p>
-          <p>You can find more of my work on <a href="https://github.com/danxschz" target="_blank" rel="noreferrer">GitHub</a></p>
-          <p>Reach me at <a href="mailto:danxschz@gmail.com">danxschz@gmail.com</a> or <a href="https://www.linkedin.com/in/danxschz/" target="_blank" rel="noreferrer">in/danxschz</a></p>
+          <p>You can find more of my work on my <a href="https://danxschz.com" target="_blank" rel="noreferrer">portfolio</a></p>
+          <p>Reach me at <a href="mailto:contact@danxschz.com">contact@danxschz.com</a> or <a href="https://www.linkedin.com/in/danxschz/" target="_blank" rel="noreferrer">in/danxschz</a></p>
         </div>
       </div>
     </main>
